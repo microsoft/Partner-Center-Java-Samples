783a8654f26f586341b96f7f8f78e5cb27219fb9
Luyunmt <2534523917@qq.com>
2020-09-23 13:56:15 +0800

    change according to comments

--- secure-app-model/keyvault/cpvsample/src/main/java/com/microsoft/store/samples/secureappmodel/cpvsample/security/KeyVaultProvider.java
+++ secure-app-model/keyvault/cpvsample/src/main/java/com/microsoft/store/samples/secureappmodel/cpvsample/security/KeyVaultProvider.java
@@ -3,8 +3,8 @@
 
 package com.microsoft.store.samples.secureappmodel.cpvsample.security;
 
-import com.azure.identity.ClientSecretCredential;
-import com.azure.identity.ClientSecretCredentialBuilder;
+import com.azure.identity.DefaultAzureCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
 
@@ -32,7 +32,7 @@ public class KeyVaultProvider implements IVaultProvider
      * @param clientSecret The secure secret of the client requesting the token.
      * @param tenantId The tenant Id of the client requesting the token. 
      */
-    public KeyVaultProvider(String vaultBaseUrl, String clientId, String clientSecret,String tenantId)
+    public KeyVaultProvider(String vaultBaseUrl, String clientId, String clientSecret, String tenantId)
     {
         client = getKeyVaultClient(clientId, clientSecret, tenantId);
         this.vaultBaseUrl = vaultBaseUrl;
@@ -71,15 +71,11 @@ public class KeyVaultProvider implements IVaultProvider
      */
     private SecretClient getKeyVaultClient(String clientId, String clientSecret, String tenantId)
     {
-        ClientSecretCredential clientSecretCredential=new ClientSecretCredentialBuilder()
-        .clientId(clientId)
-        .clientSecret(clientSecret)
-        .tenantId(tenantId)
-        .build();
+        DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredentialBuilder().build();
         
         client = new SecretClientBuilder()
         .vaultUrl(vaultBaseUrl)
-        .credential(clientSecretCredential)
+        .credential(defaultAzureCredential)
         .buildClient();
 
         return client;
